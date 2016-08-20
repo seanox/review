010c3d0ce095f8cd37e48fc8e70599ff399b845b
seanox <service@seanox.de>
2016-08-20 13:27:22 +0200

    #0000

--- test/test.java
+++ test/test.java
@@ -1,5 +1,3 @@
-import org.omg.CORBA.portable.OutputStream;
-
 /**
  *  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
  *  eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
@@ -9,6 +7,15 @@ import org.omg.CORBA.portable.OutputStream;
  *  
  *  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
  *  eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam 
+ *  voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+ *  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+ *  amet.
+ */
+import org.omg.CORBA.portable.OutputStream;
+
+/**
+ *  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+ *  eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
  *  voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
  *  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
  *  amet.
@@ -26,6 +33,10 @@ public class Test {
     private static final int validVariableInitalisation_but_invalid_name = 1;
     
     private static final int VALID_VARIABLE_INITALISATION_AND_INVALID_NAME = 1;
+    
+    private final int MISSING_STATIC_FOR_A_CONSTANT = 1;
+    
+    private static int MISSING_FINAL_FOR_A_CONSTANT = 1;
     
     static class Invalid_Class_Name {
     }
@@ -74,6 +85,9 @@ public class Test {
         boolean a = xxx ||
                 yyy &&
                 zzz;
+        
+        new Boolean(true);
+        new Boolean(false);
     }
 
     @Override
